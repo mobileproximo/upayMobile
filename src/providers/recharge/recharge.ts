901ecfc90ddde0e7cbb4b723d28f9254d7d62a08
mobileproximo <proximoatps@gmail.com>
2019-05-03 18:05:02 +0000

    change

--- src/providers/recharge/recharge.ts
+++ src/providers/recharge/recharge.ts
@@ -42,7 +42,7 @@ export class RechargeProvider {
           this.glb.recu.guichet = this.glb.IDTERM.substring(5,6);
           this.glb.recu.agence = this.glb.HEADER.agence;
           if(parametres.recharge.oper=="0074")
-          this.glb.recu.telRech = datarecharge.codeTransfert;
+          this.glb.recu.telRech = reponse.codeTransfert;
           this.glb.showRecu=true;
           this.glb.HEADER.montant= this.number.transform(reponse.mntPlfap);
           this.glb.dateUpdate = this.serv.getCurrentDate();
